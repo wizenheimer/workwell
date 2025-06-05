9c5061ea2cf88a6c9d6bf5be3eb1428563f542a4
wizenheimer <xNayanKumar@gmail.com>
2025-06-05 21:42:17 +0530

    Initial Commit

---
+++ Workwell/WorkwellApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkwellApp.swift
+//  Workwell
+//
+//  Created by Nayan on 05/06/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkwellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
